a9310a9be2130259147d0ae5c063c05368e493b0
Pomojkas <knyazkov01@bk.ru>
2023-02-03 20:31:05 +0800

    Добавьте файлы проекта.

---
+++ Osu clone/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Osu_clone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.endLabel = new System.Windows.Forms.Label();
+            this.retryLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 300;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Matura MT Script Capitals", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(12, 9);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(150, 47);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "score: 0";
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Font = new System.Drawing.Font("Matura MT Script Capitals", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerLabel.Location = new System.Drawing.Point(11, 67);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(140, 50);
+            this.timerLabel.TabIndex = 1;
+            this.timerLabel.Text = "time: 0";
+            // 
+            // endLabel
+            // 
+            this.endLabel.AutoSize = true;
+            this.endLabel.Font = new System.Drawing.Font("Matura MT Script Capitals", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.endLabel.Location = new System.Drawing.Point(241, 9);
+            this.endLabel.Name = "endLabel";
+            this.endLabel.Size = new System.Drawing.Size(321, 85);
+            this.endLabel.TabIndex = 2;
+            this.endLabel.Text = "You win!";
+            this.endLabel.Visible = false;
+            // 
+            // retryLabel
+            // 
+            this.retryLabel.AutoSize = true;
+            this.retryLabel.Font = new System.Drawing.Font("Matura MT Script Capitals", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.retryLabel.Location = new System.Drawing.Point(12, 505);
+            this.retryLabel.Name = "retryLabel";
+            this.retryLabel.Size = new System.Drawing.Size(116, 47);
+            this.retryLabel.TabIndex = 3;
+            this.retryLabel.Text = "Retry";
+            this.retryLabel.Visible = false;
+            this.retryLabel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.retryLabel_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = global::Osu_clone.Resource1.Background;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.retryLabel);
+            this.Controls.Add(this.endLabel);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Osu clone";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Label endLabel;
+        private System.Windows.Forms.Label retryLabel;
+    }
+}
+
---
+++ Osu clone/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Osu_clone
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap HandlerTexture = Resource1.Handler,
+                      TargetTexture = Resource1.Target;
+        private Point _targetPosition = new Point(400, 300);
+        private Point _direction = Point.Empty;
+        private int _score = 0;
+        private int _scoreLimit = 400;
+        private int _timer = 40;
+        private int _difficult = 35;
+        private bool _notEndScreen = true;
+
+        private void SetDefault()
+        {
+            _targetPosition = new Point(400, 300);
+            _direction = Point.Empty;
+            _score = 0;
+            _scoreLimit = 400;
+            _timer = 40;
+            _difficult = 35;
+            _notEndScreen = true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Size = new Size(800, 600);
+
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint, true);
+
+            UpdateStyles();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            timer2.Interval = rand.Next(500, 1001);
+            _direction.X = rand.Next(-1, 2);
+            _direction.Y = rand.Next(-1, 2);
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (_timer > 0 && _score < _scoreLimit)
+            {
+                _timer -= 1;
+                timerLabel.Text = "time: " + _timer.ToString();
+            }
+            else
+            {
+                EndGame();
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            int objectSize = 100;
+            int halfObjectSize = objectSize / 2;
+
+            var localPosition = this.PointToClient(Cursor.Position);
+
+            _targetPosition.X += _direction.X * 8;
+            _targetPosition.Y += _direction.Y * 8;
+
+            if (_targetPosition.X < halfObjectSize || _targetPosition.X > this.Width - halfObjectSize)
+            {
+                _direction.X *= -1;
+            }
+
+            if (_targetPosition.Y < halfObjectSize || _targetPosition.Y > this.Height - halfObjectSize)
+            {
+                _direction.Y *= -1;
+            }
+
+            Point between = new Point(localPosition.X - _targetPosition.X, localPosition.Y - _targetPosition.Y);
+            float distance = (float)Math.Sqrt((between.X * between.X) + (between.Y * between.Y));
+
+            if (distance < _difficult)
+            {
+                AddScore(1);
+            }
+
+            var handlerRect = new Rectangle(localPosition.X - halfObjectSize, localPosition.Y - halfObjectSize, +
+                objectSize, objectSize);
+            var targetRect = new Rectangle(_targetPosition.X - halfObjectSize, _targetPosition.Y - halfObjectSize, +
+                objectSize, objectSize);
+
+            g.DrawImage(HandlerTexture, handlerRect);
+            g.DrawImage(TargetTexture, targetRect);
+        }
+
+        private void AddScore(int score)
+        {
+            _score += score;
+            scoreLabel.Text = "score: " + _score.ToString();
+        }
+
+        private void retryLabel_MouseClick(object sender, MouseEventArgs e)
+        {
+            retryLabel.Visible = false;
+            endLabel.Visible = false;
+            SetDefault();
+
+            _notEndScreen = true;
+        }
+
+        private void EndGame()
+        {
+            if (_timer >= 0 && _score >= _scoreLimit && _notEndScreen)
+                endLabel.Text = "You win!";
+            else
+                endLabel.Text = "You lose(";
+
+            _notEndScreen= false;
+            endLabel.Visible = true;
+            retryLabel.Visible = true;
+        }
+    }
+}
